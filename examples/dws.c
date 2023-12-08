4a0ea81e9f9d624df49eb1587f0aa9e1b1affe96
Dave Voutila <voutilad@gmail.com>
2023-12-08 13:48:20 -0500

    Update DWS-based example.
    - pub or sub mode based on single char arg 'p' or 'c'.
    - points (hardcoded for now) to public test server
    - clean up printfs/logging

--- examples/dws.c
+++ examples/dws.c
@@ -30,70 +30,111 @@ SOFTWARE.
 #include "dws.h"
 #include <mqtt.h>
 
-static const char *msg = "Test from websocket wrapper.\n";
+
+static const char *MSG = "Test from websocket wrapper!";
+static const char *WILL_MSG = "Bye bye bye!";
+static const char *HOST = "test.mosquitto.org";
+static const char *PORT = "8080";
+
+static const char *SUB = "datetime/#";
+static char TOPIC[128];
+static const char *TOPIC_PATTERN = "datetime/%d/now";
+static char WILL[128];
+static const char *WILL_PATTERN = "datetime/%d/will";
+static char CLIENT[23];
+static const char *CLIENT_PATTERN = "dws-%u";
+
+static uint8_t BUF_SEND[4096];
+static uint8_t BUF_RECV[4096];
+
 
 void callback(void **unused, struct mqtt_response_publish *published)
 {
-    char buf[4096] = { 0 };
+    char buf[4096];
+    memset(buf, 0, sizeof(buf));
     memcpy(buf, published->topic_name, published->topic_name_size);
-    printf("%s: called, topic_name=%s, msg=%s\n", __func__, buf, (const char*) published->application_message);
+    printf("%s: topic=%s, msg=%s\n", __func__, buf,
+           (const char*) published->application_message);
 }
+
+#define MODE_PUB 1
+#define MODE_SUB 2
 
 int main(int argc, const char *argv[])
 {
-    int rv = 0;
+    int rv = 0, client_id, mode = 0;
     enum MQTTErrors result;
     struct websocket ws = { 0 };
-
-    rv = dumb_connect(&ws, "test.mosquitto.org", "8080");
-    if (rv != 0)
-        errx(1, "dumb_connect");
-
-    printf("Connected\n");
-
-    rv = dumb_handshake(&ws, "test.mosquitto.org", "/mqtt", "mqttv3.1");
-    if (rv != 0)
-        errx(1, "dumb_handshake");
-
-    printf("Completed handshake\n");
-
-    rv = dumb_ping(&ws);
-    if (rv != 0)
-        errx(1, "dumb_ping");
-
-    printf("Pinged\n");
-
-    // Start the MQTT junk
     struct mqtt_client client;
-    uint8_t sendbuf[4096];
-    uint8_t recvbuf[4096];
 
+    if (argc > 1 && argv[1][0] == 'p')
+        mode = MODE_PUB;
+    else if (argc > 1 && argv[1][0] == 's')
+        mode = MODE_SUB;
+    else
+        errx(1, "missing mode switch!");
+
+	memset(TOPIC, 0, sizeof(TOPIC));
+	memset(WILL, 0, sizeof(WILL));
+    memset(CLIENT, 0, sizeof(CLIENT));
+	memset(BUF_SEND, 0, sizeof(BUF_SEND));
+	memset(BUF_RECV, 0, sizeof(BUF_RECV));
+
+	client_id = getpid();
+	snprintf(TOPIC, sizeof(TOPIC), TOPIC_PATTERN, client_id);
+	snprintf(WILL, sizeof(WILL), WILL_PATTERN, client_id);
+    snprintf(CLIENT, sizeof(CLIENT), CLIENT_PATTERN, client_id);
+	printf("Starting client '%s' producing to topic '%s' and using will topic "
+           "'%s'\n", CLIENT, TOPIC, WILL);
+
+	/*
+	 * Websocket setup.
+	 */
+    rv = dumb_connect(&ws, HOST, PORT);
+    if (rv != 0)
+        errx(1, "dumb_connect: rv=%d", rv);
+    printf("Connected to %s:%s\n", HOST, PORT);
+
+    rv = dumb_handshake(&ws, HOST, 7114, "/mqtt", "mqttv3.1");
+    if (rv != 0)
+        errx(1, "dumb_handshake: rv=%d", rv);
+    printf("Completed Websocket handshake\n");
+
+	/*
+	 * MQTT setup.
+	 */
     mqtt_pal_socket_handle handle = &ws;
-    mqtt_init(&client, handle, sendbuf, sizeof(sendbuf), recvbuf, sizeof(recvbuf), callback);
-    result = mqtt_connect(&client, "dws-client", NULL, NULL, 0, NULL, NULL, MQTT_CONNECT_CLEAN_SESSION, 400);
-    if (result != MQTT_OK)
-        errx(1, "mqtt_connect: %s", mqtt_error_str(result));
-
+    mqtt_init(&client, handle, BUF_SEND, sizeof(BUF_SEND), BUF_RECV,
+			  sizeof(BUF_RECV), callback);
+    result = mqtt_connect(&client, CLIENT, WILL, WILL_MSG, strlen(WILL_MSG),
+                          NULL, NULL, MQTT_CONNECT_CLEAN_SESSION, 10);
+	if (result != MQTT_OK)
+        errx(1, "mqtt_connect: %s", mqtt_error_str(client.error));
     printf("Client connected\n");
 
-    mqtt_subscribe(&client, "datetime", 0);
 
-    printf("syncing\n");
-    result = mqtt_sync(&client);
-    if (result != MQTT_OK)
-        errx(1, "sync: %s", mqtt_error_str(result));
-    printf("subscribed?\n");
+    if (mode == MODE_SUB) {
+        result = mqtt_subscribe(&client, SUB, 0);
+        if (result != MQTT_OK)
+            errx(1, "mqtt_subscribe: %s", mqtt_error_str(client.error));
+        printf("Subscribed to '%s'\n", SUB);
+    }
 
-    result = mqtt_publish(&client, "datetime", msg, strlen(msg), MQTT_PUBLISH_QOS_0);
-    if (result != MQTT_OK)
-        errx(1, "mqtt_publish: %s", mqtt_error_str(result));
+    for (;;) {
+        if (mode == MODE_PUB) {
+            result = mqtt_publish(&client, TOPIC, MSG, strlen(MSG),
+							  MQTT_PUBLISH_QOS_0);
+            if (result != MQTT_OK)
+                errx(1, "mqtt_publish: %s", mqtt_error_str(client.error));
+            printf("Published to '%s': %s\n", TOPIC, MSG);
+        }
 
-    printf("Published message.\n");
-
-    for (int i=0; i < 100000; i++) {
-        mqtt_sync(&client);
-        printf("sync...\n");
-        usleep(100000U);
+        result = mqtt_sync(&client);
+        if (result != MQTT_OK) {
+            printf("error: %s\n", mqtt_error_str(client.error));
+            break;
+        }
+        usleep(1000000U);
     }
 
     rv = dumb_close(&ws);
